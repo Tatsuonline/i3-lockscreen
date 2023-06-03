7d3a9f3c309c4c8115d8b43a6dc3c33c502ddcf8
Tatsu <alexander@keemail.me>
2023-06-03 08:37:07 -0700

    These files are what is needed to create a fun lockscreen.

---
+++ lock.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Delete previous screenshot.
+rm /tmp/screen.png
+# Set lockscreen icon.
+icon="$HOME/Pictures/Icons/frozen_jotaro.png"
+tmpbg='/tmp/screen.png'
+
+(( $# )) && { icon=$1; }
+
+# Screenshot.
+scrot "$tmpbg"
+convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
+convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
+# Launch video with full screen option. Full path is necessary.
+/usr/bin/mpv --fs /home/tatsu/Programming/Scripts/lock_screen/new_za_warudo.webm
+# Lock the screen. Use "-n" to play sound after screen is unlocked.
+i3lock -u -i "$tmpbg" -n; /usr/bin/mpv /home/tatsu/Programming/Scripts/lock_screen/time-resume.mp3 
