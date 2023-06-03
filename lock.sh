c642bca69bfe847df8ad08ecbbbed9a483d0f4f1
tatsu <mail@alexanderphillips.net>
2023-06-03 09:00:09 -0700

    Expanded on a comment.

--- lock.sh
+++ lock.sh
@@ -14,5 +14,5 @@ convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
 convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
 # Launch video with full screen option. Full path is necessary.
 /usr/bin/mpv --fs /home/tatsu/Programming/Scripts/lock_screen/new_za_warudo.webm
-# Lock the screen. Use "-n" to play sound after screen is unlocked.
+# Lock the screen. Use "-n" to play sound after screen is unlocked by chaining it.
 i3lock -u -i "$tmpbg" -n; /usr/bin/mpv /home/tatsu/Programming/Scripts/lock_screen/time-resume.mp3 
